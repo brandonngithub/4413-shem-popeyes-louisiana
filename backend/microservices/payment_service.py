9e2878ce8b798b780845785253de0a9b43b8f5c1
Ricky Tran <rickie.tran7@gmail.com>
2026-04-04 17:06:24 -0400

    removed unnecessary classes

--- backend/microservices/payment_service.py
+++
@@ -1,6 +0,0 @@
-from shop.payment_strategy import PaymentStrategy
-
-
-class PaymentService:
-    def process_payment(self, strategy: PaymentStrategy, amount: float) -> bool:
-        return strategy.pay(amount)
